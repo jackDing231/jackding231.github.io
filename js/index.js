3cbcecc3ef16405bbf3fae1a426a6699fa8d765b
DingBiao_20010423 <247764891@qq.com>
2023-10-06 22:18:45 +0800

    update my project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// alert("hello world");
+
+// document.getElementById("btn").onclick = function(){
+//     alert("点击按钮");
+// }
+
+// $("#btn").click(function(){
+//     console.log("点击按钮1");
+// });
+
+// $("#btn").bind("click", function(){
+//     console.log("点击按钮2");
+// });
+// var value = prompt("请输入城市")
